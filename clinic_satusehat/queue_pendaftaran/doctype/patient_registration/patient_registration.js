5bc85fa9d4525aa831ace0e599652f45f05d6872
rakhaafd <rakhafausta07@gmail.com>
2026-09-19 13:29:36 +0700

    feat(doctype): add practitioner field and department query filter to Patient Registration

--- clinic_satusehat/queue_pendaftaran/doctype/patient_registration/patient_registration.js
+++ clinic_satusehat/queue_pendaftaran/doctype/patient_registration/patient_registration.js
@@ -2,6 +2,17 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Patient Registration', {
+	setup: function(frm) {
+		frm.set_query('practitioner', function() {
+			let filters = {
+				'status': 'Active'
+			};
+			if (frm.doc.department) {
+				filters['department'] = frm.doc.department;
+			}
+			return { filters: filters };
+		});
+	},
 	refresh: function(frm) {
 		if (!frm.is_new()) {
 			frm.add_custom_button(__('Patient Encounter'), function() {
@@ -10,6 +21,15 @@ frappe.ui.form.on('Patient Registration', {
 					frm: frm
 				});
 			}, __('Create'));
+		}
+	},
+	department: function(frm) {
+		if (frm.doc.practitioner && frm.doc.department) {
+			frappe.db.get_value('Healthcare Practitioner', frm.doc.practitioner, 'department', (r) => {
+				if (r && r.department !== frm.doc.department) {
+					frm.set_value('practitioner', '');
+				}
+			});
 		}
 	}
 });
